49fa66484ac8f41aabe6f615b7d03f6ea1677a87
Nikolay Tereshchenko <nikolay.ter@gmail.com>
2020-11-24 22:19:54 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+import IndexPage from './components/pages/IndexPage/IndexPage';
+
+function App() {
+  return (
+    <div className="App">
+      <IndexPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/pages/IndexPage/IndexPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function IndexPage() {
+  return <div className="IndexPage" />;
+}
+
+export default IndexPage;
